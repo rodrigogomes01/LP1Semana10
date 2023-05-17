b58573c47379221658f877064cf1d22d29ff0dc2
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-05-17 22:57:02 +0100

    Exercise 2
    Added classes from previous the exercise

---
+++ PlayerManager4/Player.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+namespace PlayerManager4
+{
+    public class Player : IComparable<Player>
+    {
+        public string Name { get; }
+        public int Score { get; }
+
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+        public int CompareTo(Player other)
+        {
+            if (other == null)
+                return 1;
+
+            return other.Score - Score;
+        }
+    }
+}
