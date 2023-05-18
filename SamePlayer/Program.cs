e736cee58fb884b5dbfbe8618494402aa0c91805
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-05-18 22:48:01 +0100

    Exercise 6
    Added project SamePlayer

---
+++ SamePlayer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SamePlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
